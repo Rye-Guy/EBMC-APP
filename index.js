6858a594a9e8545744e2d1c2cccab016f23f62e9
Rye-Guy <ryanracioppo95@gmail.com>
2019-07-04 17:02:15 -0400

    added data to track date and only add attendees past a certain point in time so only recently changed/added will be sent to Mailchimp

--- index.js
+++ index.js
@@ -12,96 +12,188 @@ const md5 = require('js-md5');
 //       continuation: 'eyJwYWdlIjogMn0',
 //       has_more_items: true },
 
-let attendee_batch = []
-let attendees_checked_in = []
-
-
 const chainAsync = fns => {
     let curr = 0;
     const last = fns[fns.length - 1];
     const next = () => {
-      const fn = fns[curr++];
-      fn === last ? fn() : fn(next);
+        const fn = fns[curr++];
+        fn === last ? fn() : fn(next);
     };
     next();
 };
 
-axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}`).then((res) => {
-    console.log(res.data.pagination.page_count)
+function theStuff() {
+
+    timeToRef = '2019-06-30T19:53:32.000Z'
+    timeToRef = new Date(timeToRef)
+    let attendee_batch = []
+    let attendees_checked_in = []
+
+    axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}`).then((res) => {
         chainAsync([
-        next => {
-            for (i = 1; i <= res.data.pagination.page_count; i++) {
-                axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}&page=${i}`).then((res) => {
-                    event_attendees = res.data.attendees    
-                    event_attendees.forEach(attendee => {
-                        attendee_obj = {
-                            "method": "post",
-                            "path": "/lists/864ae29f4d/members/",
-                            "body": {
-                                "email_address": attendee.profile.email,
-                                "status": "subscribed",
-                                "merge_fields": {
-                                    "FNAME": attendee.profile.first_name,
-                                    "LNAME": attendee.profile.last_name
-                                }
-                            }
+                    next => {
+                        for (i = 1; i <= res.data.pagination.page_count; i++) {
+                            axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}&page=${i}`).then((res) => {
+                                event_attendees = res.data.attendees
+                                event_attendees.forEach(attendee => {
+                                    dateChanged = attendee.changed
+                                    dateChanged = new Date(dateChanged)
+                                    console.log(timeToRef, dateChanged)
+                                    if (dateChanged > timeToRef) {
+                                        console.log('we should have special action here')
+                                        attendee_obj = {
+                                            "method": "post",
+                                            "path": "/lists/864ae29f4d/members/",
+                                            "body": {
+                                                "email_address": attendee.profile.email,
+                                                "status": "subscribed",
+                                                "merge_fields": {
+                                                    "FNAME": attendee.profile.first_name,
+                                                    "LNAME": attendee.profile.last_name
+                                                }
+                                            }
+                                        }
+                                        attendee_batch.push(attendee_obj)
+                                    }
+                                    
+                                })
+                            }).catch((err) => {
+                                console.log(err)
+                            })
                         }
-                        attendee_batch.push(attendee_obj)
-                    })
+                        console.log(attendee_batch)
+                        setTimeout(next, 5000);
+                    }, next => {
+                        console.log('5 second');
+                        console.log(attendee_batch)
+                            // mailchimp.batch(attendee_batch, {
+                            //                     wait : true,
+                            //                     interval : 1000,
+                            //                     unpack : true,
+                            //                }).then((res)=>{
+                            //                 //  console.log(res)
+                            //                }).catch((err)=>{
+                            //                    //console.log(err[1].title)
 
-                    event_attendees.forEach(attendee =>{
-                        console.log(attendee.checked_in === true)
-                        if(attendee.checked_in === true){
-                            attendee_checked_in_tag = {
-                                "method": "post",
-                                "path": `lists/864ae29f4d/members/${md5(attendee.profile.email)}/tags`,
-                                "body": {
-                                    "tags": [{
-                                        "name": "do-these-1600-or-so-in-a-batch",
-                                        "status": "active"
-                                    }]
-                                }
-                             }
-                             console.log(attendee_checked_in_tag)
-                             attendees_checked_in.push(attendee_checked_in_tag)
-                        }
-                    })
-                }).catch((err) => {
-                    //console.log(err)
-                })
-            }
-            setTimeout(next, 5000);
-            console.log(attendee_batch)
-        },
-        next => {
-            console.log('5 second');
-            mailchimp.batch(attendee_batch, {
-                        wait : true,
-                        interval : 1000,
-                        unpack : true,
-                   }).then((res)=>{
-                    //  console.log(res)
-                   }).catch((err)=>{
-                       //console.log(err[1].title)
-                    
-                   })
-            setTimeout(next, 10000);
-        },
-        next =>{
-            console.log(attendees_checked_in)
-            mailchimp.batch(attendees_checked_in,{
-                wait: true,
-                interval: 1000,
-                unpack: true
-            }).then((res)=>{
-                console.log(res)
-            }).catch((err)=>{
-                console.log(err)
-            })
+                            //                })
+                                  //  setTimeout(next, 10000);
+                                },
+            ])
         }
-        ]);
-    
-}).catch((err) => {
-    console.log(err)
-});
+    )
+    setTimeout(theStuff, 10000)
+}
+    theStuff();
 
+                            //         next => {
+                            //             console.log('5 second');
+                            //             mailchimp.batch(attendee_batch, {
+                            //                         wait : true,
+                            //                         interval : 1000,
+                            //                         unpack : true,
+                            //                    }).then((res)=>{
+                            //                     //  console.log(res)
+                            //                    }).catch((err)=>{
+                            //                        //console.log(err[1].title)
+
+                            //                    })
+                            //             setTimeout(next, 10000);
+                            //         },
+                            //         next =>{
+                            //             console.log(attendees_checked_in)
+                            //             mailchimp.batch(attendees_checked_in,{
+                            //                 wait: true,
+                            //                 interval: 1000,
+                            //                 unpack: true
+                            //             }).then((res)=>{
+                            //                 console.log(res)
+                            //             }).catch((err)=>{
+                            //                 console.log(err)
+                            //             })
+                            //         }
+                            //         ]);
+
+                            // }).catch((err) => {
+                            //     console.log(err)
+                            // });
+
+
+
+                            // axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}`).then((res) => {
+                            //     console.log(res.data.pagination.page_count)
+                            //         chainAsync([
+                            //         next => {
+                            //             for (i = 1; i <= res.data.pagination.page_count; i++) {
+                            //                 axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}&page=${i}`).then((res) => {
+                            //                     event_attendees = res.data.attendees    
+                            //                     event_attendees.forEach(attendee => {
+                            //                         attendee_obj = {
+                            //                             "method": "post",
+                            //                             "path": "/lists/864ae29f4d/members/",
+                            //                             "body": {
+                            //                                 "email_address": attendee.profile.email,
+                            //                                 "status": "subscribed",
+                            //                                 "merge_fields": {
+                            //                                     "FNAME": attendee.profile.first_name,
+                            //                                     "LNAME": attendee.profile.last_name
+                            //                                 }
+                            //                             }
+                            //                         }
+                            //                         attendee_batch.push(attendee_obj)
+                            //                     })
+
+                            //                     event_attendees.forEach(attendee =>{
+                            //                         console.log(attendee.checked_in === true)
+                            //                         if(attendee.checked_in === true){
+                            //                             attendee_checked_in_tag = {
+                            //                                 "method": "post",
+                            //                                 "path": `lists/864ae29f4d/members/${md5(attendee.profile.email)}/tags`,
+                            //                                 "body": {
+                            //                                     "tags": [{
+                            //                                         "name": "do-these-1600-or-so-in-a-batch",
+                            //                                         "status": "active"
+                            //                                     }]
+                            //                                 }
+                            //                              }
+                            //                              console.log(attendee_checked_in_tag)
+                            //                              attendees_checked_in.push(attendee_checked_in_tag)
+                            //                         }
+                            //                     })
+                            //                 }).catch((err) => {
+                            //                     //console.log(err)
+                            //                 })
+                            //             }
+                            //             setTimeout(next, 5000);
+                            //             console.log(attendee_batch)
+                            //         },
+                            //         next => {
+                            //             console.log('5 second');
+                            //             mailchimp.batch(attendee_batch, {
+                            //                         wait : true,
+                            //                         interval : 1000,
+                            //                         unpack : true,
+                            //                    }).then((res)=>{
+                            //                     //  console.log(res)
+                            //                    }).catch((err)=>{
+                            //                        //console.log(err[1].title)
+
+                            //                    })
+                            //             setTimeout(next, 10000);
+                            //         },
+                            //         next =>{
+                            //             console.log(attendees_checked_in)
+                            //             mailchimp.batch(attendees_checked_in,{
+                            //                 wait: true,
+                            //                 interval: 1000,
+                            //                 unpack: true
+                            //             }).then((res)=>{
+                            //                 console.log(res)
+                            //             }).catch((err)=>{
+                            //                 console.log(err)
+                            //             })
+                            //         }
+                            //         ]);
+
+                            // }).catch((err) => {
+                            //     console.log(err)
+                            // });
