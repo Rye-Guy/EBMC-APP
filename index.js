392f54df65a38387f7a982eddb0459346f1976f6
Rye-Guy <ryanracioppo95@gmail.com>
2019-07-04 15:27:51 -0400

    base working app that dumps a list of eventbright users to mailchimp and creates a checkedin tag if they go to the event

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const md5 = require('js-md5');
 //       continuation: 'eyJwYWdlIjogMn0',
 //       has_more_items: true },
 
+let attendee_batch = []
+let attendees_checked_in = []
+
+
 const chainAsync = fns => {
     let curr = 0;
     const last = fns[fns.length - 1];
@@ -20,51 +24,13 @@ const chainAsync = fns => {
       fn === last ? fn() : fn(next);
     };
     next();
-  };
+};
 
-function postAttendeeToMC(attendees, checked_in) {
-
-
-    // path: 'lists/1b9f43edc3/members',
-    // body: {
-    //     "email_address": attendee.profile.email,
-    //     "status": "subscribed",
-    //     "merge_fields": {
-    //         "FNAME": attendee.profile.first_name,
-    //         "LNAME": attendee.profile.last_name
-    //     }
-    // }
-
-    //  console.log(res)
-
-    // }).catch((err) => {
-    // console.log(err.title)
-    // if (err.title === 'Member Exists') {
-    //     if (checked_in === true) {
-    //     //    console.log('happening')
-    //         mailchimp.post({
-    //             path: `lists/1b9f43edc3/members/${md5(attendee.profile.email)}/tags`,
-    //             body: {
-    //                 "tags": [{
-    //                     "name": "Checked-In-Test",
-    //                     "status": "active"
-    //                 }]
-    //             }
-    //         }).then((res) => {
-    //              console.log(res)
-    //         }).catch((err) => {
-    //             console.log(err)
-    //         })
-    //     }
-    // }
-}
-let attendee_batch = []
 axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}`).then((res) => {
     console.log(res.data.pagination.page_count)
         chainAsync([
         next => {
             for (i = 1; i <= res.data.pagination.page_count; i++) {
-            
                 axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/attendees/?token=${process.env.EB_PRIVATE_API_KEY}&page=${i}`).then((res) => {
                     event_attendees = res.data.attendees    
                     event_attendees.forEach(attendee => {
@@ -82,6 +48,24 @@ axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/at
                         }
                         attendee_batch.push(attendee_obj)
                     })
+
+                    event_attendees.forEach(attendee =>{
+                        console.log(attendee.checked_in === true)
+                        if(attendee.checked_in === true){
+                            attendee_checked_in_tag = {
+                                "method": "post",
+                                "path": `lists/864ae29f4d/members/${md5(attendee.profile.email)}/tags`,
+                                "body": {
+                                    "tags": [{
+                                        "name": "do-these-1600-or-so-in-a-batch",
+                                        "status": "active"
+                                    }]
+                                }
+                             }
+                             console.log(attendee_checked_in_tag)
+                             attendees_checked_in.push(attendee_checked_in_tag)
+                        }
+                    })
                 }).catch((err) => {
                     //console.log(err)
                 })
@@ -93,19 +77,28 @@ axios.get(`https://www.eventbriteapi.com/v3/events/${process.env.EB_EVENT_ID}/at
             console.log('5 second');
             mailchimp.batch(attendee_batch, {
                         wait : true,
-                        interval : 100,
+                        interval : 1000,
                         unpack : true,
                    }).then((res)=>{
-                       console.log(res)
-            
+                    //  console.log(res)
                    }).catch((err)=>{
-                       console.log(err.title)
+                       //console.log(err[1].title)
+                    
                    })
-            console.log(attendee_batch.length)
-                   
-
+            setTimeout(next, 10000);
+        },
+        next =>{
+            console.log(attendees_checked_in)
+            mailchimp.batch(attendees_checked_in,{
+                wait: true,
+                interval: 1000,
+                unpack: true
+            }).then((res)=>{
+                console.log(res)
+            }).catch((err)=>{
+                console.log(err)
+            })
         }
-
         ]);
     
 }).catch((err) => {
